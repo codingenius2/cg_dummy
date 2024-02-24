dfa3893080edc5889365075aa3844985b8ad19e1
Codingeniu2 <Codingenius2@gmail.com>
2024-02-24 23:23:14 +0530

    update main 1.2

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const ytname = "codingenius";
 console.log("this is our channel", ytname);
 console.log("this is  our feature -2 branch.");
 console.log("this is  our feature -1 branch.");
+console.log("this is  our feature main branch.");
