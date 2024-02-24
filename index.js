fff2cb6b374d9ffadf73ef3c22373e6432511b10
Codingeniu2 <Codingenius2@gmail.com>
2024-02-24 23:24:50 +0530

    update main 1.3

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log("this is our channel", ytname);
 console.log("this is  our feature -2 branch.");
 console.log("this is  our feature -1 branch.");
 console.log("this is  our feature main branch.");
+
+const age = "26 years";
+const Des = "Software developer.";
